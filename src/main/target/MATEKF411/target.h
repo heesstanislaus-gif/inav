443a85fd928118db7891ee8ee4e9e7d91dcc14d0
heesstanislaus-gif <hees.stanislaus@gmail.com>
2026-06-05 11:31:59 +0200

    Update target.h

--- src/main/target/MATEKF411/target.h
+++ src/main/target/MATEKF411/target.h
@@ -35,15 +35,14 @@
 #define SPI1_MISO_PIN           PA6
 #define SPI1_MOSI_PIN           PA7
 
-#define USE_IMU_MPU6000
-#define IMU_MPU6000_ALIGN       CW180_DEG
-#define MPU6000_CS_PIN          PA4
-#define MPU6000_SPI_BUS         BUS_SPI1
+#define USE_IMU_ICM42605
+#define IMU_ICM42605_ALIGN      CW180_DEG
+#define ICM42605_CS_PIN         PA4
+#define ICM42605_SPI_BUS        BUS_SPI1
 
-#define USE_IMU_MPU6500
-#define IMU_MPU6500_ALIGN       CW180_DEG
-#define MPU6500_CS_PIN          PA4
-#define MPU6500_SPI_BUS         BUS_SPI1
+#define USE_EXTI
+#define GYRO_INT_EXTI           PA1
+#define USE_MPU_DATA_READY_SIGNAL
 
 // *************** SPI2 OSD *****************************
 #define USE_SPI_DEVICE_2
@@ -68,61 +67,24 @@
 #define UART2_TX_PIN            PA2
 #define UART2_RX_PIN            PA3
 
-#if defined(MATEKF411_SFTSRL2)
-#define USE_SOFTSERIAL1
-#define SOFTSERIAL_1_TX_PIN     PA0 // ST1 pad
-#define SOFTSERIAL_1_RX_PIN     PA0
-#define USE_SOFTSERIAL2
-#define SOFTSERIAL_2_TX_PIN     PA8 // LED pad
-#define SOFTSERIAL_2_RX_PIN     PA8
-#define SERIAL_PORT_COUNT       5
 
-#elif defined(MATEKF411_FD_SFTSRL)
-#define USE_SOFTSERIAL1
-#define SOFTSERIAL_1_TX_PIN     PA0 // ST1 pad
-#define SOFTSERIAL_1_RX_PIN     PA8 // LED pad
-#define SERIAL_PORT_COUNT       4
-
-#elif defined(MATEKF411_RSSI)
-#define USE_SOFTSERIAL1
-#define SOFTSERIAL_1_TX_PIN     PA8 // LED pad
-#define SOFTSERIAL_1_RX_PIN     PA8
-#define SERIAL_PORT_COUNT       4
-
-#else
-#define USE_SOFTSERIAL1
-#define SOFTSERIAL_1_TX_PIN     PA0 // ST1 pad
-#define SOFTSERIAL_1_RX_PIN     PA0
-#define SERIAL_PORT_COUNT       4
-#endif
+#define SERIAL_PORT_COUNT       3
 
 #define DEFAULT_RX_TYPE         RX_TYPE_SERIAL
-#define SERIALRX_PROVIDER       SERIALRX_SBUS
-#define SERIALRX_UART           SERIAL_PORT_USART1
+#define SERIALRX_PROVIDER       SERIALRX_CRSF
+#define SERIALRX_UART           SERIAL_PORT_USART2
+
+
 
 // *************** I2C /Baro/Mag/Pitot ********************
-#define USE_I2C
-#define USE_I2C_DEVICE_1
-#define I2C1_SCL                PB8
-#define I2C1_SDA                PB9
+// Ruibet F4AIO 12A: I2C pads exist, but onboard barometer is normally not fitted.
+// Disabled to free PB8/PB9.
+// PB9 / SDA is used as servo output in target.c.
 
-#define DEFAULT_I2C_BUS         BUS_I2C1
-
-#define USE_BARO
-#define BARO_I2C_BUS            BUS_I2C1
-#define USE_BARO_BMP280
-#define USE_BARO_MS5611
-#define USE_BARO_BMP085
-#define USE_BARO_DPS310
-#define USE_BARO_SPL06
-
-#define TEMPERATURE_I2C_BUS     BUS_I2C1
-
-#define USE_MAG
-#define MAG_I2C_BUS             BUS_I2C1
-#define USE_MAG_ALL
-
-#define PITOT_I2C_BUS           BUS_I2C1
+// no I2C
+// no BARO
+// no MAG
+// no PITOT
 
 // *************** ADC *****************************
 #define USE_ADC
@@ -133,30 +95,23 @@
 #define VBAT_ADC_CHANNEL            ADC_CHN_1
 #define CURRENT_METER_ADC_CHANNEL   ADC_CHN_2
 
-#if defined(MATEKF411_RSSI)
-#define ADC_CHANNEL_3_PIN           PA0
-#define RSSI_ADC_CHANNEL            ADC_CHN_3
-#endif
 
 // *************** LED2812 ************************
-#if !defined(MATEKF411_SFTSRL2) && !defined(MATEKF411_RSSI) && !defined(MATEKF411_FD_SFTSRL)
-#define USE_LED_STRIP
-#define WS2811_PIN                      PA8
-#endif
-// ***************  OTHERS *************************
-#define DEFAULT_FEATURES        (FEATURE_TX_PROF_SEL | FEATURE_OSD | FEATURE_CURRENT_METER | FEATURE_VBAT | FEATURE_TELEMETRY | FEATURE_SOFTSERIAL )
+// Disabled: PA8 / LED pad is used as servo output in target.c.
 
-#define USE_SPEKTRUM_BIND
-#define BIND_PIN                PA10 //  RX1
+// no LED_STRIP
+// ***************  OTHERS *************************
+#define DEFAULT_FEATURES        (FEATURE_TX_PROF_SEL | FEATURE_OSD | FEATURE_CURRENT_METER | FEATURE_VBAT | FEATURE_TELEMETRY)
 
 #define USE_DSHOT
-#define USE_ESC_SENSOR
 
-#define USE_SERIAL_4WAY_BLHELI_INTERFACE
+#define USE_GPS
+#define USE_NAV
+#define USE_WAYPOINTS
 
 #define TARGET_IO_PORTA         0xffff
 #define TARGET_IO_PORTB         0xffff
 #define TARGET_IO_PORTC         0xffff
 #define TARGET_IO_PORTD         (BIT(2))
 
-#define MAX_PWM_OUTPUT_PORTS       7
+#define MAX_PWM_OUTPUT_PORTS    9
